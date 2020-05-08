01b857774e25e3c78cfa27bd5162aa89b6716471
Israel Santos <israel.santos.cr@gmail.com>
2020-05-08 18:31:38 -0300

    Criando o projeto.

---
+++ amazingmovies/src/main/java/br/uece/eesdevops/amazingmovies/AmazingmoviesApplication.java
@@ -0,0 +1,13 @@
+package br.uece.eesdevops.amazingmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazingmoviesApplication.class, args);
+	}
+
+}
---
+++ amazingmovies/src/test/java/br/uece/eesdevops/amazingmovies/AmazingmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uece.eesdevops.amazingmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
