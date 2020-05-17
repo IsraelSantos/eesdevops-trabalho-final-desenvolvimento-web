1d0466d30f4d27a91109d755fbf214125847b8ab
Israel Santos <israel.santos.cr@gmail.com>
2020-05-16 21:46:34 -0300

    Iniciando mecanismos de avaliação.

--- amazingmovies-api/src/main/java/br/uece/eesdevops/amazingmovies/domain/service/ToEvaluateService.java
+++ amazingmovies-api/src/main/java/br/uece/eesdevops/amazingmovies/domain/service/ToEvaluateService.java
@@ -1,8 +1,27 @@
 package br.uece.eesdevops.amazingmovies.domain.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.uece.eesdevops.amazingmovies.domain.entity.Evaluation;
+import br.uece.eesdevops.amazingmovies.repository.EvaluationRepository;
+import br.uece.eesdevops.amazingmovies.repository.MovieRepository;
 
 @Service
 public class ToEvaluateService {
 
+	EvaluationRepository evaluationRepository;
+	MovieRepository movieRepository;
+	
+	@Autowired
+	ToEvaluateService(
+			EvaluationRepository evaluationRepository
+			){
+		this.evaluationRepository = evaluationRepository;
+	}
+	
+	public void execute(Evaluation evaluation) throws RuntimeException{
+		
+	}
+	
 }
